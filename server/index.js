25b5bbbe669c6d7d25bf814fad1742e9662c81c4
mossa-Sammer <mossa708496@gmail.com>
2019-10-09 15:21:30 +0300

    remove unneccssary files relates #9

--- server/index.js
+++ server/index.js
@@ -2,6 +2,5 @@ const app = require('./app');
 
 const port = app.get('port');
 app.listen(port, () => {
-  // eslint-disable-next-line no-console
   console.log(`App is on http://localhost:${port}`);
 });
